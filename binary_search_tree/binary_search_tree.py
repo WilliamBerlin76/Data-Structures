049c33ed9ab8b7b2b26c935be3813aac03efa67d
WilliamBerlin76 <william.berlin76@gmail.com>
2020-02-26 17:07:14 -0600

    lru cache from lecture, completed contains and insert from bst

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,15 +9,71 @@ class BinarySearchTree:
         self.value = value
         self.left = None
         self.right = None
+        self.root = None
 
     # Insert the given value into the tree
     def insert(self, value):
         pass
+        # check if tree contains values
+            # if not 
+                # insert value
+                # becomes root
+        if self.value == None:
+            self.value = BinarySearchTree(value)
+            return self.value
+            
+            # if so:
+        elif value >= self.value and self.right == None:
+            self.right = BinarySearchTree(value)
+            return value
+        elif value < self.value and self.left == None:
+            self.left = BinarySearchTree(value)
+            return value
+
+        elif value >= self.value and self.right != None:
+            self = self.right
+            self.insert(value)
+        elif value < self.value and self.left != None:
+            self = self.left
+            self.insert(value)
+
+                # compare value to current val:
+                # if less:
+                    # traverse to self.left
+                        # doesnt exist:
+                            # self.left is val
+                        # does exist:
+                            # repeat
+                # if more:
+                    # traverse to self.right
+                        # doesnt exist:
+                            # self.right is val
+                        # does exist:
+                            # repeat
+                    
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        elif self.right == None and self.left == None:
+            return False
+        
+        elif target > self.value and self.right != None:
+            self = self.right
+            return self.contains(target)
+        elif target > self.value and self.right == None:
+            return False
+
+        elif target < self.value and self.left != None:
+            self = self.left
+            return self.contains(target)
+        elif target < self.value and self.left == None:
+            return False
+        
+        
+
 
     # Return the maximum value found in the tree
     def get_max(self):
